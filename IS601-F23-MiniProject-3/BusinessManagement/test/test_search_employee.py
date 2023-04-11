2d73276776d9f8fcf2045442a8530be4426d0d08
Divya Mehta <dm27091996@gmail.com>
2023-04-10 21:50:58 -0400

    Changes

--- IS601-F23-MiniProject-3/BusinessManagement/test/test_search_employee.py
+++ IS601-F23-MiniProject-3/BusinessManagement/test/test_search_employee.py
@@ -44,6 +44,7 @@ def get_cell_content_by_index(index, table):
 def query_and_get_assert(query, args, target, client, url):
     from ..sql.db import DB
     result = DB.selectAll(query, *args)
+    print(result)
     if result.status and result.rows:
         n = result.rows[0][target]
         print("db result", n)
